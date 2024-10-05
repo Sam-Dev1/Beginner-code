ef6a54b3a91b227546ebfc689c4411bbd9024ec6
Samir Khatiwada <samirkhatiwada154@gmail.com>
2024-10-05 20:37:02 +0545

    WAP to Add Two Number

--- AddTwoNumber.java
+++ AddTwoNumber.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String []args){
-    int a=10 , b=2 , add;
+    int a=10 , b=2 ,add;
     add=a+b;
     System.out.println("The sum of " + a + " + " + b +" is "+add);
   }
